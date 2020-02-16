ed9a875819002ef88e3f94c8668576a62ba7b02e
Justinkokolo <59849115+Justinkokolo@users.noreply.github.com>
2020-02-16 16:53:44 +0200

    first change to main file

--- src/com/justin/Main.java
+++ src/com/justin/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git hub");
+	
+        System.out.println("Hello Git hub i made a change");
     }
 }
