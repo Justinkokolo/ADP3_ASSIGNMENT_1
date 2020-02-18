fba5910fec674e5b859976d604a25b757438628f
Justinkokolo <59849115+Justinkokolo@users.noreply.github.com>
2020-02-19 00:05:11 +0200

    commited to branch2

---
+++ src/com/justin/Calculation.java
@@ -0,0 +1,15 @@
+package com.justin;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Git hub");
+
+              int num1 = 5;
+              int num2 = 8;
+              int sum = num1+num2 ;
+         System.out.println(sum);
+
+    }
+}
